03dc0a6487d150f2cd0545ded49a1f12a12e7584
Edgar <eduardoguzmanmacedo@gmail.com>
2023-08-15 16:08:49 -0500

    15/08/2023

---
+++ condicionales/12.py
@@ -0,0 +1,18 @@
+import os
+os.system("cls")
+
+numero = int(input("Numero: "))
+
+if numero == 1 : print("Lunes")
+elif numero == 2 : print("Martes")
+elif numero == 3 : print("Miercoles")
+elif numero == 4 : print("Jueves")
+elif numero == 5 : print("Viernes")
+elif numero == 6 : print("Sabado")
+elif numero == 7 : print("Domingo")
+else : print("Error el numero es del 1 al 7")
+
+dias = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
+
+if numero in range(1,8) : print(dias[numero - 1])
+else : print("Error el numero es del 1 al 7")
